b8c647fc5c18e9198e32ee61cb41230d1e927848
Phoenix616 <mail@moep.tv>
2019-08-14 17:25:45 +0100

    Update to 1.13 and modernize

--- src/main/java/de/themoep/SimpleGamemodes/SimpleGamemodes.java
+++ src/main/java/de/themoep/SimpleGamemodes/SimpleGamemodes.java
@@ -2,23 +2,22 @@ package de.themoep.SimpleGamemodes;
 
 /**
  * SimpleGamemode - Gamemode management Bukkit plugin
- Copyright (C) 2015 Max Lee (https://github.com/Phoenix616/)
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.* 
+ * Copyright (C) 2015 Max Lee (https://github.com/Phoenix616/)
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
  */
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
@@ -30,7 +29,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -45,25 +43,22 @@ public class SimpleGamemodes extends JavaPlugin implements Listener, CommandExec
     final static List<GameMode> GM_PRIO = Arrays.asList(new GameMode[]{GameMode.CREATIVE, GameMode.SURVIVAL, GameMode.ADVENTURE, GameMode.SPECTATOR});
 
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(this, this);
     }
-    
+
     @EventHandler
-     public void onGameJoin(PlayerJoinEvent event) {
-        if(!event.getPlayer().hasPermission("simplegamemodes.autogamemodeexempt")) {
+    public void onGameJoin(PlayerJoinEvent event) {
+        if (!event.getPlayer().hasPermission("simplegamemodes.autogamemodeexempt")) {
             final UUID id = event.getPlayer().getUniqueId();
-            Bukkit.getScheduler().scheduleSyncDelayedTask(this, new BukkitRunnable() {
-                @Override
-                public void run() {   
-                    Player p = Bukkit.getPlayer(id);
-                    if(p != null && p.isOnline()) {
-                        if(!p.hasPermission("simplegamemodes.gamemode." + p.getGameMode().toString().toLowerCase())) {
-                            for (GameMode gm : GM_PRIO) {
-                                if (p.hasPermission("simplegamemodes.gamemode." + gm.toString().toLowerCase())) {
-                                    p.setGameMode(gm);
-                                    Bukkit.getServer().getLogger().info("Automatically set gamemode of " + p.getName() + " to " + gm.toString() + " on game join.");
-                                    p.sendMessage(ChatColor.RED + "Automatically set your gamemode to " + ChatColor.YELLOW + humanizeEnum(gm) + ChatColor.RED + "!");
-                                }
+            getServer().getScheduler().runTaskLater(this, () -> {
+                Player p = getServer().getPlayer(id);
+                if (p != null && p.isOnline()) {
+                    if (!p.hasPermission("simplegamemodes.gamemode." + p.getGameMode().toString().toLowerCase())) {
+                        for (GameMode gm : GM_PRIO) {
+                            if (p.hasPermission("simplegamemodes.gamemode." + gm.toString().toLowerCase())) {
+                                p.setGameMode(gm);
+                                getLogger().info("Automatically set gamemode of " + p.getName() + " to " + gm.toString() + " on game join.");
+                                p.sendMessage(ChatColor.RED + "Automatically set your gamemode to " + ChatColor.YELLOW + humanizeEnum(gm) + ChatColor.RED + "!");
                             }
                         }
                     }
@@ -74,20 +69,17 @@ public class SimpleGamemodes extends JavaPlugin implements Listener, CommandExec
 
     @EventHandler
     public void onGameJoin(PlayerChangedWorldEvent event) {
-        if(!event.getPlayer().hasPermission("simplegamemodes.autogamemodeexempt")) {
+        if (!event.getPlayer().hasPermission("simplegamemodes.autogamemodeexempt")) {
             final UUID id = event.getPlayer().getUniqueId();
-            Bukkit.getScheduler().scheduleSyncDelayedTask(this, new BukkitRunnable() {
-                @Override
-                public void run() {
-                    Player p = Bukkit.getPlayer(id);
-                    if(p != null && p.isOnline()) {
-                        if(!p.hasPermission("simplegamemodes.gamemode." + p.getGameMode().toString().toLowerCase())) {
-                            for (GameMode gm : GM_PRIO) {
-                                if (p.hasPermission("simplegamemodes.gamemode." + gm.toString().toLowerCase())) {
-                                    p.setGameMode(gm);
-                                    Bukkit.getServer().getLogger().info("Automatically set gamemode of " + p.getName() + " to " + gm.toString() + " on world change.");
-                                    p.sendMessage(ChatColor.RED + "Automatically set your gamemode to " + ChatColor.YELLOW + humanizeEnum(gm) + ChatColor.RED + "!");
-                                }
+            getServer().getScheduler().runTaskLater(this, () -> {
+                Player p = getServer().getPlayer(id);
+                if (p != null && p.isOnline()) {
+                    if (!p.hasPermission("simplegamemodes.gamemode." + p.getGameMode().toString().toLowerCase())) {
+                        for (GameMode gm : GM_PRIO) {
+                            if (p.hasPermission("simplegamemodes.gamemode." + gm.toString().toLowerCase())) {
+                                p.setGameMode(gm);
+                                getLogger().info("Automatically set gamemode of " + p.getName() + " to " + gm.toString() + " on world change.");
+                                p.sendMessage(ChatColor.RED + "Automatically set your gamemode to " + ChatColor.YELLOW + humanizeEnum(gm) + ChatColor.RED + "!");
                             }
                         }
                     }
@@ -116,16 +108,16 @@ public class SimpleGamemodes extends JavaPlugin implements Listener, CommandExec
             } else {
                 return false;
             }
-            
+
         } else if (cmd.getName().equalsIgnoreCase("checkgamemode") && sender.hasPermission("simplegamemodes.command.checkgamemode")) {
-            if(args.length == 0) {
-                if(sender instanceof Player)
+            if (args.length == 0) {
+                if (sender instanceof Player)
                     sender.sendMessage(ChatColor.RED + "Your gamemode is " + ChatColor.YELLOW + humanizeEnum(((Player) sender).getGameMode()));
                 else
                     sender.sendMessage("This command can only be run by a player without arguments. Use " + cmd.getName() + "<player...> to run it from the console!");
-            } else if(args.length > 0 && sender.hasPermission("simplegamemodes.command.checkgamemode.others")) {
+            } else if (args.length > 0 && sender.hasPermission("simplegamemodes.command.checkgamemode.others")) {
                 for (String s : args) {
-                    Player p = Bukkit.getPlayer(s);
+                    Player p = getServer().getPlayer(s);
                     if (p != null && p.isOnline()) {
                         sender.sendMessage(ChatColor.RED + "Player " + ChatColor.YELLOW + p.getName() + ChatColor.RED + " is in " + ChatColor.YELLOW + humanizeEnum(p.getGameMode()) + ChatColor.RED + " mode!");
                     } else {
@@ -135,26 +127,26 @@ public class SimpleGamemodes extends JavaPlugin implements Listener, CommandExec
             } else {
                 sender.sendMessage("You don't have permission to check the gamemode of other players!");
             }
-            
+
         } else if (sender.hasPermission("simplegamemodes.command.gamemode")) {
-            
+
             GameMode gm;
-            if(cmd.getName().equalsIgnoreCase("gmc")) {
+            if (cmd.getName().equalsIgnoreCase("gmc")) {
                 gm = GameMode.CREATIVE;
-            } else if(cmd.getName().equalsIgnoreCase("gms")) {
+            } else if (cmd.getName().equalsIgnoreCase("gms")) {
                 gm = GameMode.SURVIVAL;
-            } else if(cmd.getName().equalsIgnoreCase("gma")) {
+            } else if (cmd.getName().equalsIgnoreCase("gma")) {
                 gm = GameMode.ADVENTURE;
-            } else if(cmd.getName().equalsIgnoreCase("gmsp")) {
+            } else if (cmd.getName().equalsIgnoreCase("gmsp")) {
                 gm = GameMode.SPECTATOR;
             } else {
                 return false;
             }
-            
-            if(sender.hasPermission("simplegamemodes.gamemode." + gm.toString().toLowerCase())) {
+
+            if (sender.hasPermission("simplegamemodes.gamemode." + gm.toString().toLowerCase())) {
                 if (args.length > 0 && sender.hasPermission("simplegamemodes.command.gamemode.others")) {
                     for (String s : args) {
-                        Player p = Bukkit.getPlayer(s);
+                        Player p = getServer().getPlayer(s);
                         if (p != null && p.isOnline()) {
                             if (p.getGameMode() != gm) {
                                 p.setGameMode(gm);
@@ -182,7 +174,7 @@ public class SimpleGamemodes extends JavaPlugin implements Listener, CommandExec
             } else {
                 sender.sendMessage("You don't have permission to access the gamemode " + humanizeEnum(gm) + "!");
             }
-            
+
         } else {
             sender.sendMessage("You don't have permission to run this command!");
         }
